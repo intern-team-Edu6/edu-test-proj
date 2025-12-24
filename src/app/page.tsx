be97212317cac29bfb6306cb683ba7210b1b1bc2
Khishigdari <nbhishgee22@gmail.com>
2025-12-24 11:57:39 +0800

    leaflet first trial 12.24

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import JamkaPage from "@/components/jamka/page";
 import React from "react";
+import Map from "./_components/Map";
 
-const HomePage = () => {
+export default function HomePage() {
   return (
-    <div>
+    <div style={{ height: "100vh", width: "100%" }}>
       <JamkaPage />
+      <Map />
     </div>
   );
-};
-export default HomePage;
+}
